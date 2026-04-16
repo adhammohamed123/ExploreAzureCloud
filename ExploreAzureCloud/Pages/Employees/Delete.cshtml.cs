6ba34a2266e1f4d32db082472674704ddc91ab5b
adhammohamed123 <adhammo909@gmail.com>
2026-04-16 23:56:31 +0200

    add some of generated ui from models

---
+++ ExploreAzureCloud/Pages/Employees/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ExploreAzureCloud.Data;
+
+namespace ExploreAzureCloud.Pages.Employees
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ExploreAzureCloud.Data.ApplicationDbContext _context;
+
+        public DeleteModel(ExploreAzureCloud.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (employee is not null)
+            {
+                Employee = employee;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee != null)
+            {
+                Employee = employee;
+                _context.Employees.Remove(Employee);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ExploreAzureCloud/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExploreAzureCloud.Data;
+
+namespace ExploreAzureCloud.Pages.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly ExploreAzureCloud.Data.ApplicationDbContext _context;
+
+        public EditModel(ExploreAzureCloud.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee =  await _context.Employees.FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            Employee = employee;
+           ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(Employee.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool EmployeeExists(Guid id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
